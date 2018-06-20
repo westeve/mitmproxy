4f94b51cb0e908a1cb8d7abb16fe0fab0d624ee5
westeve <39627977+westeve@users.noreply.github.com>
2018-06-20 15:50:33 -0400

    Update dev.sh
    remove references to virtualenv

--- dev.sh
+++ dev.sh
@@ -4,8 +4,3 @@ set -x
 
 pip3 install -U pip setuptools
 pip3 install -r requirements.txt
-
-echo ""
-echo "  * Created virtualenv environment in ./venv."
-echo "  * Installed all dependencies into the virtualenv."
-echo "  * You can now activate the $(python3 --version) virtualenv with this command: \`. venv/bin/activate\`"
